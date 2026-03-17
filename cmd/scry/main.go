d0c5e9176bdc1ebec6e1330d5c8c0cd6249fe381
Harry Y. <harryyu02@gmail.com>
2026-03-17 13:31:31 -0400

    wip: create cli entry point to read from scraper/data and feed to indexer

--- cmd/scry/main.go
+++ cmd/scry/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"encoding/json"
+
+	"github.com/HarryYu02/scry/internal/indexer"
+	"github.com/HarryYu02/scry/internal/parser"
+)
+
 
 func main() {
+	file, err := os.Open("scraper/data/sts.jsonl")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	sourceDocs := make([]parser.Document, 0)
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		docBytes := fileScanner.Bytes()
+		var docContent parser.Document
+		err := json.Unmarshal(docBytes, &docContent)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+		sourceDocs = append(sourceDocs, docContent)
+	}
+
+	docs, err := indexer.Search([]parser.Document{}, "", 3)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
 }
---
+++ internal/indexer/indexer.go
@@ -0,0 +1,11 @@
+package indexer
+
+
+import (
+	"github.com/HarryYu02/scry/internal/parser"
+)
+
+
+func Search(source []parser.Document, query string, count int) ([]parser.Document, error) {
+	return []parser.Document{}, nil
+}
